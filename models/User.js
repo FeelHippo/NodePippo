7544db491b288787a463ed9026f8de0e2e14f35b
FeelHippo <mezzasegolas@gmail.com>
2020-04-27 21:11:07 +0200

    added locals and auth config

---
+++ models/User.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const nodemailerTrans = require('../lib/nodemailerConfig');
+
+// define schema
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        unique: true
+    },
+    password: String,
+});
+
+userSchema.statics.hashPassword = function(plainPassword) {
+    return bcrypt.hash(plainPassword, 10);
+}
+
+userSchema.methods.sendEmail = function(from, subject, body) {
+    
+    // send an email, async operation
+    return nodemailerTrans.sendMail({
+        from: from,
+        to: this.email,
+        subject: subject,
+        html: body
+    })
+}
+
+// use schema to create a model
+const User = mongoose.model('User', userSchema);
+
+// export model
+module.exports = User;
